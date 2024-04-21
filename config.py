f9e526745959b9a472b72e0836404c7e02f8ecd0
Annika Sinha <131460984+annikaasinha@users.noreply.github.com>
2024-04-21 20:11:31 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,15 @@
+env_config = {
+    'parameter1': 'value1',
+    'parameter2': 'value2'
+}
+
+agent_config = {
+    'gamma': 0.95,  # discount rate
+    'epsilon': 1.0,  # exploration rate
+    'epsilon_min': 0.01,
+    'epsilon_decay': 0.995,
+    'learning_rate': 0.001
+}
+
+num_episodes = 1000
+max_steps = 200
